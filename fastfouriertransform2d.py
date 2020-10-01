dfcb1f1b6e5555c37a31e5848f060b422081c006
Alperen <mralp762@gmail.com>
2020-10-01 18:34:00 +0300

    made script fit standart python practices

--- fastfouriertransform2d.py
+++ fastfouriertransform2d.py
@@ -11,17 +11,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
 
-plt.figure(figsize=(20, 4.8*5), constrained_layout=False)
+plt.figure(figsize=(20, 4.8 * 5), constrained_layout=False)
 
 img = cv2.imread("ben.jpg", 0)
 
-def distance(point1,point2):
+def distance(point1, point2):
     return sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
-def idealFilterLP(D0,imgShape):
+def ideal_filter_LP(D0, imgShape):
     base = np.zeros(imgShape[:2])
     rows, cols = imgShape[:2]
-    center = (rows/2,cols/2)
+    center = (rows / 2, cols / 2)
     for x in range(cols):
         for y in range(rows):
             if distance((y,x),center) < D0:
@@ -35,14 +35,14 @@ plt.subplot(151), plt.imshow(img, "gray"), plt.title("Original Image")
 
 #plt.subplot(151), plt.imshow(np.log(1+np.abs(center)), "gray"), plt.title("Spectrum")
 
-LowPass = idealFilterLP(50,img.shape)
-plt.subplot(152), plt.imshow(np.abs(LowPass), "gray"), plt.title("Low Pass Filter")
+low_pass = ideal_filter_LP(50,img.shape)
+plt.subplot(152), plt.imshow(np.abs(low_pass), "gray"), plt.title("Low Pass Filter")
 
-LowPassCenter = center * idealFilterLP(25,img.shape)
-plt.subplot(153), plt.imshow(np.log(1+np.abs(LowPassCenter)), "gray"), plt.title("Centered Spectrum multiply Low Pass Filter")
+low_pass_center = center * ideal_filter_LP(25,img.shape)
+plt.subplot(153), plt.imshow(np.log(1+np.abs(low_pass_center)), "gray"), plt.title("Centered Spectrum multiply Low Pass Filter")
 
-LowPass = np.fft.ifftshift(LowPassCenter)
-plt.subplot(154), plt.imshow(np.log(1+np.abs(LowPass)), "gray"), plt.title("Decentralize")
+low_pass = np.fft.ifftshift(low_pass_center)
+plt.subplot(154), plt.imshow(np.log(1+np.abs(low_pass)), "gray"), plt.title("Decentralize")
 
-inverse_LowPass = np.fft.ifft2(LowPass)
-plt.subplot(155), plt.imshow(np.abs(inverse_LowPass), "gray"), plt.title("Processed Image")
+inverse_low_pass = np.fft.ifft2(low_pass)
+plt.subplot(155), plt.imshow(np.abs(inverse_low_pass), "gray"), plt.title("Processed Image")
